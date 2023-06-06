24712135950f5b7b8002c796f635ec56efd895d3
Sbarrachina <sandrabarrachina1978@gmail.com>
2023-06-06 20:57:31 +0200

    agregando ejemplos creando constantes con y sin arrays

---
+++ constantes.php
@@ -0,0 +1,30 @@
+<?php 
+
+// la manera antigua de definir constante
+
+ define("NOMBRE","Carlos");
+
+ echo NOMBRE;
+
+ // la manera moderna de definir constante
+
+ 
+
+?>
+
+<?php 
+
+// la manera moderna de definir constante
+ const NAME= "Ashley";
+
+ echo NAME;
+
+?>
+
+<?php 
+
+// definiendo una constante utilizando un Array con datos
+define ("ALUMNOS",array("Carlos","Alba","Emma"));
+
+echo ALUMNOS [2];
+?>
