c19a7c9250b9abf53a11e3371e6137f82c5939e9
Sbarrachina <sandrabarrachina1978@gmail.com>
2023-06-09 14:25:14 +0200

    agregando ejemplos de operadores incremento

---
+++ operadores_incremento.php
@@ -0,0 +1,54 @@
+<?php 
+
+$numero=10;
+
+// Pre-incremento
+echo ++$numero; //imprime 11 incrementa el valor de una variable
+
+//Post-incremento
+
+
+?>
+
+<br>
+
+<?php 
+
+$numero=10;
+
+//Post-incremento
+
+echo $numero++;// imprime 10 realiza la acción y luego autoincrementar
+echo $numero; // imprime 11
+?>
+
+<br>
+
+<?php 
+
+$numero=10;
+
+// Pre-decremento
+echo --$numero; //imprime 9 resta uno al valor de la variable
+
+?>
+
+<?php 
+
+$numero=10;
+
+// Post-decremento
+echo $numero--; //imprime 10 realiza la operación y luego decrementa
+echo $numero; // imprime 9
+
+?>
+
+<br>
+<?php 
+$numero=10;
+
+
+$resultado=++$numero;
+echo $resultado; // imprime 11 
+
+?>
